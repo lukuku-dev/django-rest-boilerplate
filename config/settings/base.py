fb7b3d1dd86d91ac902d18cc320857ab9e3ba042
sohaibanwaar <sohaibanwaar36@gmail.com>
2023-02-13 12:12:24 +0500

    add: Adding the swagger documentaion

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'allauth',
     'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from django.urls import path
 
 from django.http import JsonResponse
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': '<PROJECT NAME>',
+    'DESCRIPTION': '<PROJECT DESCRIPTION>',
+    'VERSION': '0.0.1',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'CAMELIZE_NAMES': False,
+    'SERVE_PERMISSIONS': [],
+    
+    # OTHER SETTINGS
+}
 
 def hello_world_view(request):
     data = {'message': 'hello world'}
@@ -14,6 +25,6 @@ def hello_world_view(request):
 urlpatterns = [
     path('', hello_world_view),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
 ]
