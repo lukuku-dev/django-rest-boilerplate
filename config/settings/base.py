2f5567c194171cecc593517220d7242b4c4adaad
Lukuku <novelview9@gmail.com>
2023-02-13 16:27:49 +0900

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'allauth',
     'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
