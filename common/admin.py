bf04dc746b26bfc6a12a4bcc8d99c5e355f7f3d9
donguk lee <novelview9@gmail.com>
2023-08-09 12:05:40 +0900

    Update admin refectoring

--- common/admin.py
+++ common/admin.py
@@ -27,32 +27,22 @@ class ModelAdmin(admin.ModelAdmin):
 
     @staticmethod
     def check_related_field(field):
-        if type(field) in [ForeignObjectRel, ManyToOneRel, OneToOneRel]:
-            return True
+        return isinstance(field, (ForeignObjectRel, ManyToOneRel, OneToOneRel))
 
     def get_list_display(self, request):
-        if self.list_display == ('__str__', ):
-            list_display = [
-                field.name for field in self.model._meta.get_fields()
-                if not self.check_related_field(field)
-            ]
-            if 'created' in list_display:
-                list_display.remove('created')
-                list_display.append('created')
-            if 'modified' in list_display:
-                list_display.remove('modified')
-                list_display.append('modified')
+        list_display = self.list_display
 
-        else:
-            list_display = self.list_display
+        if list_display == ('__str__', ):
+            list_display = [field.name for field in self.model._meta.get_fields() if not self.check_related_field(field)]
+            for field_name in ['created', 'modified']:
+                if field_name in list_display:
+                    list_display.remove(field_name)
+                    list_display.append(field_name)
 
         if hasattr(self, 'additional_list_display'):
-            list_display += self.additional_list_display
-
-        if hasattr(self, 'exclude_list_display'):
-            list_display = [
-                field for field in list_display
-                if field not in self.exclude_list_display
-            ]
+            list_display.extend(self.additional_list_display)
+        
+        exclude_list_display = getattr(self, 'exclude_list_display', ())
+        list_display = [field for field in list_display if field not in exclude_list_display]
 
         return list_display
